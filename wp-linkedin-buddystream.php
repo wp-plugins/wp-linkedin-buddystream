b817d0268567dd1dee5827fe3d7b92c71cf8f1a3
cvedovini <cvedovini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-21 13:55:14 +0000

    changed the way the extension plugs in so it can cache everything locally
    git-svn-id: https://plugins.svn.wordpress.org/wp-linkedin-buddystream/trunk@808242 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-linkedin-buddystream.php
+++ wp-linkedin-buddystream.php
@@ -23,32 +23,51 @@ Version: 0.1
 # See the GNU lesser General Public License for more details.
 */
 
-define('BUDDYSTREAM_LINKEDIN_OAUTHTOKEN', 'buddystream_linkedin_token');
 
+class WPLinkedInBuddystreamConnection extends WPLinkedInConnection {
 
-function wp_linkedin_buddystream_filter_oauth_token($token) {
-	$user_token = false;
-	$user = bp_displayed_user_id();
-
-	if ($user) {
-		$user_token = get_user_meta($user, BUDDYSTREAM_LINKEDIN_OAUTHTOKEN, true);
+	public function __construct($user_id) {
+		$this->app_key = get_site_option('buddystream_linkedin_consumer_key');
+		$this->app_secret = get_site_option('buddystream_linkedin_consumer_secret');
+		$this->user_id = $user_id;
 	}
 
-	return ($user_token) ? $user_token : $token;
-}
-
-add_filter('linkedin_oauthtoken', 'wp_linkedin_buddystream_filter_oauth_token');
-
-
-
-function wp_linkedin_buddystream_filter_cachekey($cachekey) {
-	$user = bp_displayed_user_id();
-
-	if ($user) {
-		return sha1($cachekey . $user);
+	protected function set_cache($key, $value, $expires=0) {
+		if ($key == 'wp-linkedin_oauthtoken') $key = 'buddystream_linkedin_token';
+		return update_user_option($this->user_id, $key, $value);
 	}
 
-	return $cachekey;
+	protected function get_cache($key, $default=false) {
+		if ($key == 'wp-linkedin_oauthtoken') $key = 'buddystream_linkedin_token';
+		$value = get_user_option($key, $this->user_id);
+		return ($value !== false) ? $value : $default;
+	}
+
+	protected function delete_cache($key) {
+		if ($key == 'wp-linkedin_oauthtoken') return false;
+		return delete_user_option($this->user_id, $key);
+	}
+
+	protected function send_invalid_token_email() {
+		if (LINKEDIN_SENDMAIL_ON_TOKEN_EXPIRY && !get_cache('wp-linkedin_invalid_token_mail_sent')) {
+			$user_info = get_userdata($this->user_id);
+			$blog_name = get_option('blogname');
+			$user_email = $user_info->user_email;
+			$header = array("From: $blog_name <$admin_email>");
+			$subject = "[$blog_name] " . __('Invalid or expired LinkedIn access token', 'wp-linkedin-buddystream');
+
+			$message = __("The access token for the LinkedIn API is either invalid or has expired, please click on the following link to renew it.%s\n\nThis link will only be valid for a limited period of time.\n-Thank you.", 'wp-linkedin-buddystream');
+			$message = sprintf($message, $this->get_authorization_url());
+
+			$sent = wp_mail($user_email, $subject, $message, $header);
+			set_cache('wp-linkedin_invalid_token_mail_sent', $sent);
+		}
+	}
+
 }
 
-add_filter('linkedin_cachekey', 'wp_linkedin_buddystream_filter_cachekey');
+function wp_linkedin_buddystream_connection($linkedin) {
+	$user_id = bp_displayed_user_id();
+	return $user_id ? $linkedin : WPLinkedInBuddystreamConnection($user_id);
+}
+add_filter('linkedin_connection', 'wp_linkedin_buddystream_connection');
