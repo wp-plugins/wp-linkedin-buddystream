3f37b7a7bbbe1ffe9bca9306f9eadfc2806770ba
cvedovini <cvedovini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-18 07:29:46 +0000

    1.0 release
    git-svn-id: https://plugins.svn.wordpress.org/wp-linkedin-buddystream/tags/1.0@824430 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-linkedin-connection.php
@@ -0,0 +1,59 @@
+<?php
+
+
+class WPLinkedInBuddystreamConnection extends WPLinkedInConnection {
+
+	public function __construct($user_id) {
+		$this->app_key = get_site_option('buddystream_linkedin_consumer_key');
+		$this->app_secret = get_site_option('buddystream_linkedin_consumer_secret');
+		$this->user_id = $user_id;
+	}
+
+	protected function set_cache($key, $value, $expires=0) {
+		if ($key == 'wp-linkedin_oauthtoken') {
+			$key = 'buddystream_linkedin_token';
+			delete_user_option($this->user_id, 'buddystream_linkedin_reauth');
+		}
+
+		return update_user_option($this->user_id, $key, $value);
+	}
+
+	protected function get_cache($key, $default=false) {
+		if ($key == 'wp-linkedin_oauthtoken') {
+			$key = 'buddystream_linkedin_token';
+		}
+
+		$value = get_user_option($key, $this->user_id);
+		return ($value !== false) ? $value : $default;
+	}
+
+	protected function delete_cache($key) {
+		if ($key == 'wp-linkedin_oauthtoken') {
+			update_user_option($this->user_id, 'buddystream_linkedin_reauth', true);
+			return false;
+		}
+
+		return delete_user_option($this->user_id, $key);
+	}
+
+	public function get_token_process_url() {
+		return bp_core_get_user_domain($this->user_id) . BP_SETTINGS_SLUG . '/buddystream-networks/?network=linkedin';
+	}
+
+	protected function send_invalid_token_email() {
+		if (LINKEDIN_SENDMAIL_ON_TOKEN_EXPIRY && !get_cache('wp-linkedin_invalid_token_mail_sent')) {
+			$user_info = get_userdata($this->user_id);
+			$blog_name = get_option('blogname');
+			$user_email = $user_info->user_email;
+			$header = array("From: $blog_name <$admin_email>");
+			$subject = "[$blog_name] " . __('Invalid or expired LinkedIn access token', 'wp-linkedin-buddystream');
+
+			$message = __("The access token for the LinkedIn API is either invalid or has expired, please click on the following link to renew it.%s\n\nThis link will only be valid for a limited period of time.\n-Thank you.", 'wp-linkedin-buddystream');
+			$message = sprintf($message, $this->get_authorization_url());
+
+			$sent = wp_mail($user_email, $subject, $message, $header);
+			set_cache('wp-linkedin_invalid_token_mail_sent', $sent);
+		}
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== WP LinkedIn/BuddyStream integration ===
+Author: Claude Vedovini
+Contributors: cvedovini
+Donate link: http://vedovini.net/plugins/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-linkedin-buddystream
+Tags: linkedin,resume,recommendations,profile,buddystream,buddypress
+Requires at least: 2.7
+Tested up to: 3.8
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+
+== Description ==
+
+This plugin integrates the WP-LinkedIn and BuddyStream plugins and enables showing more than one profile per install.
+
+
+== Installation ==
+
+This plugin follows the [standard WordPress installation method](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins):
+
+1. Upload the `wp-linkedin-buddystream` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Changelog ==
+
+= Version 1.0 =
+- Initial release.
